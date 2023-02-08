819dc2a2b7d7ecd08ded0c5773096cadc6f91075
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-02-08 13:45:18 +0000

    adding w12 d3 more content

---
+++ week_12/day_3/inheritance/inheritance_start_point/src/main/java/Instructor.java
@@ -0,0 +1,13 @@
+public class Instructor extends Person {
+
+    private String moduleTeam;
+
+    public Instructor(String name, String cohort, String moduleTeam) {
+        super(name, cohort);
+        this.moduleTeam = moduleTeam;
+    }
+
+    public String getModuleTeam(){
+        return moduleTeam;
+    }
+}
