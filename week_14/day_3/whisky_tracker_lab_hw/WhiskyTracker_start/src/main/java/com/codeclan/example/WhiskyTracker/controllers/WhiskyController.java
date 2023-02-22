f63aa02f4aebaa9b1c2390510af75a6d43e0959c
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-02-22 22:36:33 +0000

    adding d3 whisky traker lab/hw

---
+++ week_14/day_3/whisky_tracker_lab_hw/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,39 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam Optional<Integer> whiskyYear, @RequestParam Optional<String> distilleryName, @RequestParam Optional<Integer> whiskyAge, @RequestParam Optional<String> whiskyRegion) {
+        if(whiskyYear.isPresent()) {
+            return new ResponseEntity<>(whiskyRepository.findByYear(whiskyYear.get()), HttpStatus.OK);
+        } else if (distilleryName.isPresent() && whiskyAge.isPresent()){
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distilleryName.get(), whiskyAge.get()), HttpStatus.OK);
+        } else if (whiskyRegion.isPresent()) {
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryRegion(whiskyRegion.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+
+}
---
+++ week_14/day_3/whisky_tracker_lab_hw/WhiskyTracker_start/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,57 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.jws.Oneway;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //Indicates it's a test profile so will not run DataLoader
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canGetWhiskiesForYear() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByYear(1995);
+		assertEquals(5, foundWhiskies.size());
+	}
+
+	@Test
+	public void canGetDistilleryForRegion() {
+		List<Distillery> foundDistilleries = distilleryRepository.findByRegion("Highland");
+		assertEquals(15, foundDistilleries.size());
+	}
+
+	@Test
+	public void canGetWhiskiesForDistilleryForAge() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByDistilleryNameAndAge("Macallan", 25);
+		assertEquals(1, foundWhiskies.size());
+	}
+
+	@Test
+	public void canGetWhiskiesForRegion() {
+		List<Whisky> foundWhiskies = whiskyRepository.findByDistilleryRegion("Highland");
+		assertEquals(7, foundWhiskies.size());
+	}
+
+}
